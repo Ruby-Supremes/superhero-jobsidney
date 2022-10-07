6c80eae97ca8b8d558c851ac1094786df44f2f9e
Jobsidney <jobsidney67@gmail.com>
2022-10-07 16:58:44 +0300

    heroes seriaizer and routes

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -5,4 +5,10 @@ class HerosController < ApplicationController
         heros=Hero.all
         render json: heros, status: :ok
     end
+
+    def show
+        hero=Hero.find_by(id:params[:id])
+            render json: hero, status: :ok
+
+    end
 end
