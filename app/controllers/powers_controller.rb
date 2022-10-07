50f05cfb96ceed8e7a998d7b4d3b3f13b0a7ea94
Jobsidney <jobsidney67@gmail.com>
2022-10-07 22:21:17 +0300

    patch powers

--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -13,4 +13,21 @@ class PowersController < ApplicationController
             render json: {error: "Power not found"}, status: :not_found  
         end
     end
+
+    def update
+        power=Power.find_by(id: params[:id])
+        if power
+            power.update(name: params[:name], description: params[:description])
+            render json: power
+        else
+            render json: {error: "error"}, status: :unprocessable_entity
+        end
+
+    end
+
+    private
+
+    def power_params
+        params.permit(:name,:description)
+    end
 end
