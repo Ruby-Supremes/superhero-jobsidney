ff8b24bf92f7f2e414e5c3a3640e1ce6dd1d2c84
Jobsidney <jobsidney67@gmail.com>
2022-10-07 15:30:48 +0300

    Associations to models

--- app/models/hero_power.rb
+++ app/models/hero_power.rb
@@ -1,2 +1,8 @@
 class HeroPower < ApplicationRecord
+
+    validates :strength, presence: true,inclusion: {in: ['Strong','Weak','Average']}
+    
+    belongs_to :heros
+    belongs_to :powers
+    
 end
